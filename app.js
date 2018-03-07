7dc4d11836caa2cf4c563c04914eabbbb7daa464
NikhilWASD <nikhilgadkari07@gmail.com>
2018-03-07 19:26:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 (function () {
     'use strict';
-    angular.module("LunchCheck", ['ngMessages'])
-        .controller("LunchCheckController", LunchCheckController);
-
+    
+    angular.module('LunchCheck', [])
+    
+    .controller('LunchCheckController', LunchCheckController);
+    
     LunchCheckController.$inject = ['$scope'];
-
-    function LunchCheckController($scope) {
-        $scope.addItemList = function () {
-            var list = $scope.foodItem;
-            $scope.foodCount = $scope.foodItem.split(',').length;
-            var foodCount = list.length;
-            if (foodCount <= 3) {
-                $scope.result = "Enjoy";
-            } else if (foodCount > 3) {
-                $scope.result = "Too Much";
-            }
-        };
+    
+       function LunchCheckController ($scope){
+      $scope.menuItems="";
+      $scope.message="";
+    
+      $scope.checkMenuItems = function(){
+    
+       
+       var items = $scope.menuItems.split(",");
+    
+       var num_items = ValidateMenuItems(items);
+    
+       if (num_items === 0)
+          return $scope.message = "Please enter data first";
+    
+       if (num_items > 3)
+          return $scope.message = "Too much!";
+       else
+          return $scope.message = "Enjoy!";
+      };
     }
-})();
+    
+    
+    function ValidateMenuItems (items){
+      var cnt = 0;
+            for(var i=0; i<items.length; i++){
+                if(!(isEmpty(items[i]))){
+                    cnt+= 1 ;
+                }
+            }
+            return cnt;
+        }
+    
+        function isEmpty(val){
+        return (val === undefined || val == null || val.length <= 0) ? true : false;
+        }
+    
+    })();
